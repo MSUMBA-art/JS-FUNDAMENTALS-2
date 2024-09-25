d80e8a1524eed08a01020b228138172eb27c7f16
ALEXANDER <msumba83@gmail.com>
2024-09-25 17:02:54 +0200

    function declarations & expressions

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'strict';
 // ===== FUNCTIONS ======
-function logger() {
+/*function logger() {
   console.log('i am Alexander');
 }
 
@@ -17,7 +17,21 @@ const appleJuice = fruitProcessor(5, 10);
 console.log(appleJuice);
 
 const appleOrOrangeJuice = fruitProcessor(2, 9);
-console.log(appleOrOrangeJuice);
+console.log(appleOrOrangeJuice);*/
 
+////// function declaration
+function calcAge1(birthday) {
+  return 2024 - birthday;
+}
 
+const age1 = calcAge1(1983);
+// console.log(age1);
+
+/////  function expression
+const calcAge2 = function (birthYear) {
+  return 2024 - birthYear;
+};
+
+const age2 = calcAge2(1999)
+console.log(age1, age2);
 
