9efec50013a0c1e61a5e3cb0004e483b3d0e5058
ALEXANDER <msumba83@gmail.com>
2024-09-29 08:41:16 +0200

    done function calling function

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const appleOrOrangeJuice = fruitProcessor(2, 9);
 console.log(appleOrOrangeJuice);*/
 
 ////// function declaration
-function calcAge1(birthday) {
+/*function calcAge1(birthday) {
   return 2024 - birthday;
 }
 
@@ -34,4 +34,37 @@ const calcAge2 = function (birthYear) {
 
 const age2 = calcAge2(1999)
 console.log(age1, age2);
+*/
+
+
+/*const calcAge3 = birthYear => 2024 - birthYear;
+const age = calcAge3(1983)
+console.log(age);
+
+const yearsUntilRetire = (birthYear, firstName) => {
+  const age = 2024 - birthYear;
+  const retirement = 65 - age;
+  // return retirement
+  return `${firstName} retires in ${retirement} years`
+}
+
+console.log(yearsUntilRetire(1983, "Alexander"));
+console.log(yearsUntilRetire(2012, 'Irene'));*/
+
+function cutFourPieces(fruit) {
+  return fruit * 4
+}
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFourPieces(apples);
+  const orangePieces = cutFourPieces(oranges);
+  const juice = `juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+
+
+
+
+
 
